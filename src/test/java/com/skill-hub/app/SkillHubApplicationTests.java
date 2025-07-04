181051d495c8356c923950850da97ad74bbf1b09
mbcodes-hub <mb.digital.dev@gmail.com>
2025-07-04 11:11:54 +0100

    Initial commit

---
+++ src/test/java/com/skill-hub/app/SkillHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SkillHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
