49095e01f78ccaeaa5928e16116a09bcf6d92d02
mbcodes-hub <mb.digital.dev@gmail.com>
2025-07-19 11:22:36 +0100

    refactor

---
+++ backend/src/test/java/com/skill-hub/SkillHubApplicationTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
